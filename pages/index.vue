98573509699db4ff1a8f4a786017a06c08372b2d
Norikazu Teraguchi <norimaki.tera@gmail.com>
2020-05-04 19:25:20 +0900

    [add] nuxt-svg-loaderとnuxt-user-agentを追加

--- pages/index.vue
+++ pages/index.vue
@@ -1,22 +1,25 @@
 <template>
   <div class="container">
 
-<!--    <model-viewer-->
-<!--      ar-->
-<!--      ar-scale="fixed"-->
-<!--      alt="test"-->
-<!--      src="/model/rooo02.gltf"-->
-<!--      ios-src="/model/rooo01.usdz"-->
-<!--      style="width:100%;height:100%;"-->
-<!--      background-color="#70BCD1"-->
-<!--      shadow-intensity="0.7"-->
-<!--      camera-controls-->
-<!--      interaction-prompt="auto"-->
-<!--      auto-rotate ar magic-leap>-->
-<!--    </model-viewer>-->
+    <model-viewer
+      ar
+      ar-scale="fixed"
+      alt="test"
+      src="/model/rooo02.gltf"
+      ios-src="/model/rooo01.usdz"
+      style="width:100%;height:100%;"
+      background-color="#70BCD1"
+      shadow-intensity="0.7"
+      camera-controls
+      interaction-prompt="auto"
+      interaction-prompt-display="none"
+      auto-rotate ar magic-leap>
+      <button slot="ar-button" style="background-color: white; border-radius: 4px; border: none; position: absolute; top: 16px; right: 16px; ">
+        👋 Activate AR
+      </button>
+    </model-viewer>
 
 <!--    <DetailModal></DetailModal>-->
-
 
   </div>
 </template>
@@ -70,6 +73,7 @@
     top: 0;
     left: 0;
     z-index: 10000;
+    background-color: orange;
   }
 
 
